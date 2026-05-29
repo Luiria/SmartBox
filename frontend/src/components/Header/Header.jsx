824a3d583d6d4c3819cae1d2e07e6c335662f52e
Luiria <262813799+Luiria@users.noreply.github.com>
2026-05-29 22:14:13 +0200

    feat: add css + header & footer

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+ export default function Header() {
+
+    return (
+
+     <header className={styles.header}>
+        <h1 className={styles.title}>
+            SmartBox
+        </h1>
+     </header>
+    )
+    } 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import EventList from "../components/EventList/EventList.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Header from "../components/Header/Header.jsx";
 import Notification from "../components/Notification/Notification.jsx";
 import { useLatestNewEvent } from "../hook/useLatestNewEvent.js";
 
+import styles from "./Home.jsx";
+
 export default function Home() {
   const { notifications } = useLatestNewEvent();
-  console.log(notifications);
 
   return (
-    <>
+    <div className={styles.home}>
+      <Header />
       <Notification notifications={notifications} />
-      <EventList />
-    </>
+      
+      <main className={styles.main}>
+        <EventList />
+      </main>
+      <Footer />
+    </div>
   );
 }
