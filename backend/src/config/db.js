2cdecb700f2c44d1d6eb7429fd9d79aafae5a7ba
Luiria <262813799+Luiria@users.noreply.github.com>
2026-06-03 23:40:57 +0200

    fix: container don't start

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -11,15 +11,28 @@ export const db = mysql.createPool({
     queueLimit: 0
 });
 
-export async function testConnection() {
-    try {
-        const conn = await db.getConnection();
-        conn.release();
-        console.log("[DB] Connection successful");
-    } catch (error) {
-        console.error("[DB] Connection failed", error);
-        process.exit(1);
+
+export async function testConnection(retries = 30, delay = 2000) {
+
+    for (let i = 0; i < retries; i++) {
+        try {
+            const conn = await db.getConnection();
+            conn.release();
+
+            console.log("[DB] Connection successful");
+            return;
+        } catch (err) {
+
+            if (i === retries - 1) {
+                console.error("[DB] Connection failed permanently", err);
+                process.exit(1);
+            }
+
+            await new Promise((r) => setTimeout(r, delay));
+
+        }
     }
+
 }
 
 export default db;
